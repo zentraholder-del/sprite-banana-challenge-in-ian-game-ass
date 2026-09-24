bf69347be8be7a19f18d35a0220f9fe95d2f8d1c
zentraholder-del <zentraholder@gmail.com>
2026-09-24 01:52:41 +0100

    add remaining root game files

---
+++ games/sw.js
@@ -0,0 +1,8 @@
+// NOT YET TESTED. CAN / WILL BE REMOVED IF NEEDED.
+
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11187760
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
